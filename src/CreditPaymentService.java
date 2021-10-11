f4b271f0fa55abe852e700ef60b0dc048426d29b
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-10-11 13:19:07 +0300

    Second Commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,8 @@
 public class CreditPaymentService {
-    public int calculate(int creditAmount, double interestRate, int creditPeriod) {
-       int monthlyPayment = (int) (creditAmount * (interestRate / 12 / 100 + ((interestRate / 12 / 100) / (Math.pow(1 + interestRate / 12 / 100, creditPeriod) - 1))));
+    public int calculate(int creditAmount, double annualInterestRate, int creditPeriod) {
+        double monthlyInterestRate = annualInterestRate / 12 / 100;
+        double annuityPaymentRatio = monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, creditPeriod) - 1));
+        int monthlyPayment = (int) (creditAmount * annuityPaymentRatio);
         return monthlyPayment;
     }
 }
